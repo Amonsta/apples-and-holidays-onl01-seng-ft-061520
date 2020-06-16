38b59c31be4b3ded44ff44194130932e2c34988d
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-16 21:44:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   
   holiday_hash.collect do |season,holiday|
-    holiday.
+    holiday.collect do |holiday, supply|
     
 end
 
